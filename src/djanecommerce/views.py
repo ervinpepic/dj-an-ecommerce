b4417eb1458f1e4dd069bef99e8bd7bedc1a5176
Ervin Pepic <ervinpepic@ervin.local>
2018-01-31 21:53:05 +0100

    Added ability to handle input data from form. Added python forms and so more

--- src/djanecommerce/views.py
+++ src/djanecommerce/views.py
@@ -1,6 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
 
 def home_page(request):
-	return render(request, "home_page.html", {})
+	context = {
+		"title": "Hello World!",
+		"content": "Welcome to home page."
+	}
+	return render(request, "home_page.html", context)
+
+def about_page(request):
+	context = {
+		"title": "About Page!",
+		"content": "Welcome to about page."
+	}
+	return render(request, "home_page.html", context)
+
+def contact_page(request):
+	contact_form = ContactForm(request.POST or None)
+
+	context = {
+		"title": "Contact Page!",
+		"content": "Welcome to contact page.",
+		"form": contact_form
+	}
+	if contact_form.is_valid():
+		print(contact_form.cleaned_data)
+	# if request.method == "POST":
+	# 	print(request.POST.get('fullname'))
+	# 	print(request.POST.get('email'))
+	# 	print(request.POST.get('content'))
+	return render(request, "contact/view.html", context)
