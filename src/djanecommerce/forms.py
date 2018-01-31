cbc32a75fa0a20c187f5eb9499a24a17cf90085f
Ervin Pepic <ervinpepic@ervin.local>
2018-01-31 22:23:04 +0100

    Added superuser and added ability to login user

--- src/djanecommerce/forms.py
+++ src/djanecommerce/forms.py
@@ -10,3 +10,8 @@ class ContactForm(forms.Form):
 		if not "gmail.com" in email:
 			raise forms.ValidationError("Only google user can do this.")
 		return email
+
+
+class LoginForm(forms.Form):
+	username = forms.CharField()
+	password = forms.CharField(widget=forms.PasswordInput)
