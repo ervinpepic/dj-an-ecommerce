c2a6d9083e7e6fb301bb6d13f9d164d3ebdabaed
Ervin Pepic <ervin.hack@gmail.com>
2018-02-06 23:52:34 +0100

    Added slug as par of custom slug generator

--- src/djanecommerce/urls.py
+++ src/djanecommerce/urls.py
@@ -27,6 +27,7 @@ from products.views import (
     product_detail_view,
     ProductFeaturedListView,
     ProductFeaturedDetailView,
+    ProductDetailSlugView
     )
 
 # relative import from views in src
@@ -48,7 +49,8 @@ urlpatterns = [
     url(r'^featured/$', ProductFeaturedListView.as_view()),
     url(r'^featured/(?P<pk>\d+)/$', ProductFeaturedDetailView.as_view()),
     url(r'^products-fbv/$', product_list_view),
-    url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
+    # url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
+    url(r'^products/(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view()),
     url(r'^products-fbv/(?P<pk>\d+)/$', product_detail_view),
     url(r'^admin/', admin.site.urls),
 ]
--- src/products/models.py
+++ src/products/models.py
@@ -1,9 +1,12 @@
 ##Django imports
 from django.db import models
+from django.db.models.signals import pre_save, post_save
 import random
 import os
 
+
 ##Other Imports
+from .utils import unique_slug_generator
 
 #image uploading functions for renaming
 def get_filename_ext(filepath):
@@ -47,6 +50,7 @@ class ProductManager(models.Manager):
 #Model class definition
 class Product(models.Model):
 	title 			= models.CharField(max_length=120)
+	slug  			= models.SlugField(default='slug-url-share', blank=True, unique=True)
 	description 	= models.TextField()
 	price 			= models.DecimalField(max_digits=20, decimal_places=2, default=29.99)
 	image 			= models.ImageField(upload_to=upload_image_path, null=True, blank=True)
@@ -59,3 +63,9 @@ class Product(models.Model):
 	##class methods
 	def __str__(self):
 		return self.title
+
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+	if not instance.slug:
+		instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
